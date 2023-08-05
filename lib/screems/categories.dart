cc872c44cea60149e6a07f9b3580a40647237525
Harshal <190540107113@darshan.ac.in>
2023-08-05 16:48:45 +0530

    first commit

---
+++ lib/screems/categories.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:food_receipe_app/data/dummy_data.dart';
+import 'package:food_receipe_app/model/category.dart';
+import 'package:food_receipe_app/screems/meals.dart';
+import 'package:food_receipe_app/widgets/category_grid_item.dart';
+
+import '../model/meal.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen(
+      {super.key,
+      required this.onToggleFavorite,
+      required this.availableMeals});
+
+  final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
+
+  // TODO BuildContext context is used because the context in stateless widget is not available globally
+  // like in stateful widgets.
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: EdgeInsets.all(16),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: [
+        // TODO Instead of using for loop below we can also use availableCategories.map((category) => CategoryGridItem(category: category).toList())
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          ),
+      ],
+    );
+  }
+}
